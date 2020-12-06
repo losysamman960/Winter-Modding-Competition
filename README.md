238fa9d30de69cdff6d3ddd1d1f859a57476d566
losysamman960 <72412328+losysamman960@users.noreply.github.com>
2020-12-07 01:06:13 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Winter-Modding-Competition
+ This is the repository for the Winter Modding Competition Hosted By TurtyWurty
---
+++ WinterModdingCompetition/src/main/java/com/losysamman960/wintermod/TheWinteryYear.java
@@ -0,0 +1,34 @@
+package com.losysamman960.wintermod;
+
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.util.stream.Collectors;
+
+@Mod("winter")
+public class TheWinteryYear
+{
+    private static final Logger LOGGER = LogManager.getLogger();
+    public static final String MOD_ID = "winter";
+
+    public TheWinteryYear() {
+        // Register the setup method for modloading
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::setup);
+        FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void setup(final FMLCommonSetupEvent event) {
+
+    }
+
+    private void doClientStuff(final FMLClientSetupEvent event) {
+
+    }
+}
