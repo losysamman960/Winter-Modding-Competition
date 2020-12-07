684c944e8e4f364a12e7ddaeec46009b34ff4954
losysamman960 <72412328+losysamman960@users.noreply.github.com>
2020-12-07 07:39:40 +0300

    Added DeferredRegistry in ModItems.java

--- WinterModdingCompetition/src/main/java/com/losysamman960/wintermod/init/ModItems.java
+++ WinterModdingCompetition/src/main/java/com/losysamman960/wintermod/init/ModItems.java
@@ -1,4 +1,11 @@
 package com.losysamman960.wintermod.init;
 
+import com.losysamman960.wintermod.TheWinteryYear;
+import net.minecraft.item.Item;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class ModItems {
+
+    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, TheWinteryYear.MOD_ID);
 }
