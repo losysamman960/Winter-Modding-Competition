c08ecdb266fed36224696a583f27d47755ca5894
losysamman960 <72412328+losysamman960@users.noreply.github.com>
2020-12-07 07:39:22 +0300

    Added DeferredRegistry in ModFluids.java

--- WinterModdingCompetition/src/main/java/com/losysamman960/wintermod/init/ModFluids.java
+++ WinterModdingCompetition/src/main/java/com/losysamman960/wintermod/init/ModFluids.java
@@ -1,4 +1,12 @@
 package com.losysamman960.wintermod.init;
 
+import com.losysamman960.wintermod.TheWinteryYear;
+import net.minecraft.fluid.Fluid;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+
 public class ModFluids {
+
+    public static final DeferredRegister<Fluid> FLUIDS = new DeferredRegister<>(ForgeRegistries.FLUIDS, TheWinteryYear.MOD_ID);
+
 }
