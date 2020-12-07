76f9ca9d9ec345076d2b3f0324ee8dd07982bc13
losysamman960 <72412328+losysamman960@users.noreply.github.com>
2020-12-07 07:38:41 +0300

    Fixed DeferredRegistry in ModBlocks.java

--- WinterModdingCompetition/src/main/java/com/losysamman960/wintermod/init/ModBlocks.java
+++ WinterModdingCompetition/src/main/java/com/losysamman960/wintermod/init/ModBlocks.java
@@ -1,12 +1,11 @@
 package com.losysamman960.wintermod.init;
 
 import com.losysamman960.wintermod.TheWinteryYear;
-import net.minecraft.item.Item;
-import net.minecraftforge.fml.RegistryObject;
+import net.minecraft.block.Block;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModBlocks {
 
-    public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, TheWinteryYear.MOD_ID);
+    public static final DeferredRegister<Block> BLOCKS = new DeferredRegister<>(ForgeRegistries.BLOCKS, TheWinteryYear.MOD_ID);
 }
