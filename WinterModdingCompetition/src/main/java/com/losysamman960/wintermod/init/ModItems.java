c6605d4b70caf9f0ddf1ec71a5dde0d17bdf85c9
losysamman960 <72412328+losysamman960@users.noreply.github.com>
2020-12-13 23:59:40 +0300

    Added Eggnog Item

--- WinterModdingCompetition/src/main/java/com/losysamman960/wintermod/init/ModItems.java
+++ WinterModdingCompetition/src/main/java/com/losysamman960/wintermod/init/ModItems.java
@@ -1,11 +1,15 @@
 package com.losysamman960.wintermod.init;
 
 import com.losysamman960.wintermod.TheWinteryYear;
+import com.losysamman960.wintermod.items.Eggnog;
 import net.minecraft.item.Item;
+import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModItems {
 
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, TheWinteryYear.MOD_ID);
+
+    public static final RegistryObject<Item> EGG_NOG = ITEMS.register("eggnog", Eggnog::new);
 }
