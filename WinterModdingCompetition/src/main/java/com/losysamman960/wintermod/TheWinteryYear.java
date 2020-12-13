7d8229d4e37e7931cedcf69b3ad486b49e353ab5
losysamman960 <72412328+losysamman960@users.noreply.github.com>
2020-12-14 00:00:12 +0300

    Added A new Tab

--- WinterModdingCompetition/src/main/java/com/losysamman960/wintermod/TheWinteryYear.java
+++ WinterModdingCompetition/src/main/java/com/losysamman960/wintermod/TheWinteryYear.java
@@ -1,5 +1,9 @@
 package com.losysamman960.wintermod;
 
+import com.losysamman960.wintermod.init.ModItems;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
@@ -21,6 +25,7 @@ public class TheWinteryYear
         FMLJavaModLoadingContext.get().getModEventBus().addListener(this::doClientStuff);
 
         MinecraftForge.EVENT_BUS.register(this);
+        ModItems.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
     }
 
     private void setup(final FMLCommonSetupEvent event) {
@@ -30,4 +35,10 @@ public class TheWinteryYear
     private void doClientStuff(final FMLClientSetupEvent event) {
 
     }
+    public static final ItemGroup TAB = new ItemGroup("The Wintery Month") {
+        @Override
+        public ItemStack createIcon() {
+            return new ItemStack(ModItems.EGG_NOG.get());
+        }
+    };
 }
