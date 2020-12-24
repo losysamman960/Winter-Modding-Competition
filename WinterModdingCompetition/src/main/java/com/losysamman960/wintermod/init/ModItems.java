0067a0dd1bd6e8d3ef38c7e92fdbeecc0f4961d3
losysamman960 <72412328+losysamman960@users.noreply.github.com>
2020-12-24 23:16:15 +0300

    Added Eggnog Bucket and the Eggnog Bucket texture

--- WinterModdingCompetition/src/main/java/com/losysamman960/wintermod/init/ModItems.java
+++ WinterModdingCompetition/src/main/java/com/losysamman960/wintermod/init/ModItems.java
@@ -2,6 +2,7 @@ package com.losysamman960.wintermod.init;
 
 import com.losysamman960.wintermod.TheWinteryYear;
 import com.losysamman960.wintermod.items.Eggnog;
+import net.minecraft.item.BucketItem;
 import net.minecraft.item.Item;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -12,4 +13,6 @@ public class ModItems {
     public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, TheWinteryYear.MOD_ID);
 
     public static final RegistryObject<Item> EGG_NOG = ITEMS.register("eggnog", Eggnog::new);
+    public static final RegistryObject<Item> EGGNOG_BUCKET = ITEMS.register("eggnog_bucket",
+            () -> new BucketItem(() -> ModFluids.EGGNOG_FLUID.get(), new Item.Properties().group(TheWinteryYear.TAB).maxStackSize(1)));
 }
