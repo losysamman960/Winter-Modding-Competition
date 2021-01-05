590c9d24fe64e0b48e266f867f57358fe808ba70
losysamman960 <72412328+losysamman960@users.noreply.github.com>
2021-01-05 19:22:07 +0300

    added icy fence gate

--- WinterModdingCompetition/src/main/java/com/losysamman960/wintermod/blocks/IcyWall.java
+++ WinterModdingCompetition/src/main/java/com/losysamman960/wintermod/blocks/IcyWall.java
@@ -3,10 +3,11 @@ package com.losysamman960.wintermod.blocks;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.FenceBlock;
+import net.minecraft.block.WallBlock;
 
 
-public class IcyFence extends FenceBlock {
-    public IcyFence() {
+public class IcyWall extends WallBlock {
+    public IcyWall() {
         super(Block.Properties.from(Blocks.BRICK_WALL));
     }
 }
